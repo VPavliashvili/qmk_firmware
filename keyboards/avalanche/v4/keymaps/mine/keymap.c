e7395fef775edf13de13cd3a6c6b004e4e13cd31
VPavliashvili <v_pavliashvili@yahoo.com>
2025-04-04 19:54:17 +0400

    three rows implemented fully

--- keyboards/avalanche/v4/keymaps/mine/keymap.c
+++ keyboards/avalanche/v4/keymaps/mine/keymap.c
@@ -12,9 +12,11 @@ enum layers {
     _GG, // gaming
 };
 
+#define T_LANG S(KC_LALT)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MN] = LAYOUT(
-                 KC_PSCR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_DEL ,
+                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                  KC_TAB , KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         XXXXXXX, KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, XXXXXXX,
                  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LALT, MO(_FN), MO(_MS), KC_ESC , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
@@ -22,21 +24,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_SM] = LAYOUT(
                  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                 _______, XXXXXXX, XXXXXXX, KC_LBRC, KC_LCBR, XXXXXXX,                                     XXXXXXX, KC_RCBR, KC_RBRC, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, _______, KC_GRV , KC_BSLS, KC_PIPE, KC_TILD, XXXXXXX,                                     XXXXXXX, KC_EQL , KC_PLUS, KC_MINS, KC_UNDS, XXXXXXX, XXXXXXX,
+                 _______, KC_GRV , KC_TILD, KC_LBRC, KC_LCBR, XXXXXXX,                                     XXXXXXX, KC_RCBR, KC_RBRC, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, T_LANG , KC_BSLS, KC_QUOT, KC_DQUO, KC_PIPE, XXXXXXX,                                     XXXXXXX, KC_EQL , KC_PLUS, KC_MINS, KC_UNDS, XXXXXXX, XXXXXXX,
                  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
                                             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
     [_NM] = LAYOUT(
                  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                 _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                                     KC_RPRN, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, XXXXXXX,
+                 _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, XXXXXXX,
         XXXXXXX, _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0   , XXXXXXX, XXXXXXX,
                  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-                                            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+                                            _______, _______, KC_LGUI, _______, _______, _______, _______, _______, _______, _______
     ),
     [_FN] = LAYOUT(
                  _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                                     KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,
-                 _______, KC_PGDN, KC_HOME, KC_END , KC_PGUP, XXXXXXX,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                 _______, KC_PSCR, XXXXXXX, XXXXXXX, XXXXXXX, KC_DEL ,                                     XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END , XXXXXXX,
         XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX, XXXXXXX, XXXXXXX,
                  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, KC_F12 , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
                                             _______, TG(_GG), _______, _______, _______, _______, _______, KC_RALT, _______, _______

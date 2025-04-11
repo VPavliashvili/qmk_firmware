00b9b40f82bb59fe99a29b142e1de54328698205
VPavliashvili <v_pavliashvili@yahoo.com>
2025-04-12 03:02:55 +0400

    rotary encoder fault fix

--- keyboards/avalanche/v4/keymaps/mine/keymap.c
+++ keyboards/avalanche/v4/keymaps/mine/keymap.c
@@ -24,24 +24,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_SM] = LAYOUT(
                  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                 _______, KC_GRV , KC_TILD, KC_LBRC, KC_LCBR, XXXXXXX,                                     XXXXXXX, KC_RCBR, KC_RBRC, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, T_LANG , KC_BSLS, KC_QUOT, KC_DQUO, KC_PIPE, XXXXXXX,                                     XXXXXXX, KC_EQL , KC_PLUS, KC_MINS, KC_UNDS, XXXXXXX, XXXXXXX,
-                 _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+                 _______, KC_GRV , KC_TILD, KC_LBRC, KC_LCBR, _______,                                     _______, KC_RCBR, KC_RBRC, _______, _______, _______,
+        XXXXXXX, T_LANG , KC_BSLS, KC_QUOT, KC_DQUO, KC_PIPE, _______,                                     _______, KC_EQL , KC_PLUS, KC_MINS, KC_UNDS, _______, XXXXXXX,
+                 _______, _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, _______, _______, _______, _______, _______, _______,
                                             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
     [_NM] = LAYOUT(
                  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                 _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, XXXXXXX,
-        XXXXXXX, _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                     KC_6   , KC_7   , KC_8   , KC_9,    KC_0   , XXXXXXX, XXXXXXX,
-                 _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-                                            _______, _______, KC_LGUI, _______, _______, _______, _______, _______, _______, _______
+                 _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+        XXXXXXX, _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                     KC_6   , KC_7   , KC_8   , KC_9,    KC_0   , _______, XXXXXXX,
+                 _______, _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, _______, _______, _______, _______, _______, _______,
+                                            _______, _______, XXXXXXX, _______, _______, _______, _______, _______, _______, _______
     ),
     [_FN] = LAYOUT(
                  _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                                     KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,
                  _______, KC_PSCR, XXXXXXX, XXXXXXX, XXXXXXX, KC_DEL ,                                     XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END , XXXXXXX,
         XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX, XXXXXXX, XXXXXXX,
                  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, KC_F12 , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-                                            _______, TG(_GG), _______, _______, _______, _______, _______, KC_RALT, _______, _______
+                                            _______, TO(_GG), _______, _______, _______, _______, _______, KC_RALT, _______, _______
     ),
     [_MS] = LAYOUT(
                  _______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______,
@@ -51,17 +51,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             _______, _______, _______, MS_BTN3, _______, _______, _______, _______, _______, _______
     ),
     [_GG] = LAYOUT( // TODO
-                 _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                                     KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,
-                 _______, KC_GRV , XXXXXXX, XXXXXXX, XXXXXXX, KC_TILD,                                     KC_EQL , KC_PLUS, KC_HOME, KC_END , KC_LBRC, KC_RBRC,
-        XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX, XXXXXXX, XXXXXXX,
-                 _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, KC_F12 , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-                                            _______, TG(_MN), _______, _______, _______, _______, _______, _______, _______, _______
+                 KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                     KC_6   , KC_7   , KC_8   , KC_9,    KC_0   , KC_MINS,
+                 KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                                     KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS,
+        XXXXXXX, KC_BSPC, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                                     KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, XXXXXXX,
+                 KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , _______, _______, _______, _______, KC_N   , KC_M   , KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                            _______, TO(_MN), KC_SPC , _______, _______, _______, _______, KC_ENT , _______, _______
     ),
 };
 
 #ifdef ENCODER_ENABLE
 bool encoder_update_user(uint8_t index, bool clockwise) {
+    static int i = 0;
+
     if (index == 0) {
+        // my specific encoder which I already soldered on pcb
+        // is faulty and activates twice instead of once per cycle
+        // making it skip every second signal is the fix
+        i++;
+        if (i % 2 == 0) {
+            return false;
+        }
+
         if (clockwise) {
             tap_code_delay(KC_VOLU, 100);
         } else {
